47c0105ae84bf8b1b0e7105d14a9f72f4914206e
Ujjwal Jindal <jindalujjwal0720@gmail.com>
2023-09-04 23:06:36 +0530

    bugs fixed

--- src/context/Logs.jsx
+++ src/context/Logs.jsx
@@ -139,7 +139,9 @@ const LogsProvider = ({ children }) => {
       const files = decodedURL.split("files=")[1].split(",");
       files.forEach((file) => {
         const a = document.createElement("a");
-        a.href = `http://localhost:3001/download?cluster=${clusterRef.current.value}&app=${appRef.current.value}&date=${dateRef.current.value}&time=${timeRef.current.value}&pod=${podRef.current.value}&files=${file}`;
+        a.href = encodeURI(
+          `http://localhost:3001/download?cluster=${clusterRef.current.value}&app=${appRef.current.value}&date=${dateRef.current.value}&time=${timeRef.current.value}&pod=${podRef.current.value}&files=${file}`
+        );
         a.download = file;
         document.body.appendChild(a);
         a.click();
